370c4165dee04e15235af2692b4aafcca28243b9
Shahar Kaminsky <sk@matterlabs.dev>
2023-02-27 17:59:16 -0700

    Links + todos.

--- src/circuit_structures/utils.rs
+++ src/circuit_structures/utils.rs
@@ -63,6 +63,7 @@ pub fn constraint_bit_length<E: Engine, CS: ConstraintSystem<E>>(
         Num::Constant(value) => {
             let bits = value.into_repr().num_bits() as usize;
             assert!(bits <= num_bits);
+            todo!();
         }
     };
 
